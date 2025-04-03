5dbcfe0a0845d794c45806684707e815bbbdd00a
DnJ0408 <oyj921205@gmail.com>
2025-04-03 20:38:58 +0900

    feat:#3 수파베이스 User, Contacts, Plans 테이블 타입 선언

--- src/types/contacts.ts
+++ src/types/contacts.ts
@@ -16,10 +16,10 @@ export interface Contact {
   user_id: string;
   name: string;
   relationship_level: string;
-  email?: string;
-  notes?: string;
-  phone?: string;
-  birth?: string;
+  email: string;
+  notes: string;
+  phone: string;
+  birth: string;
   contact_profile_img?: string;
 }
 
@@ -31,9 +31,33 @@ export interface Plan {
   start_date:string;
   end_date: string;
   title: string;
-  detail?: string;
+  detail: string;
   priority: 'low' | 'medium' | 'high';
+}
+
+// 연락처와 약속을 함께 반환하는 타입
+export interface ContactWithPlans {
+  contact: Contact;
+  plans: Plan[];
 }
 
 // 리스트용 연락처 타입
 export type ContactList = Pick<Contact, 'contacts_id' | 'name' | 'relationship_level' | 'contact_profile_img'>;
+
+// 연락처 상세 정보 타입
+export type ContactDetail = Pick<
+  Contact, 
+  'contacts_id' | 'user_id' | 'name' | 'email' | 'relationship_level' | 'notes' | 'phone' | 'birth' | 'contact_profile_img'
+>;
+
+// 약속 상세정보 타입
+export type PlanDetail = Pick<
+  Plan,
+  'plan_id' | 'title' | 'start_date' | 'end_date' | 'priority' | 'detail'
+>;
+
+// 연락처 상세정보와 약속 상세 정보를 함께 반환하는 타입
+export interface ContactWithPlansDetail {
+  contact: ContactDetail;
+  plans: PlanDetail[];
+}
