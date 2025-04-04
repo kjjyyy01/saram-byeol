573684d670ed5016a01a62e69d39413f1c7cd015
JinChaeKim <rlawlsco88@gmail.com>
2025-04-04 18:02:27 +0900

    feat:#5 일정 데이터 적용

--- src/components/commons/calendar.tsx
+++ src/components/commons/calendar.tsx
@@ -3,6 +3,9 @@ import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
 import { format, parse, startOfWeek, getDay } from 'date-fns';
 import { enUS } from 'date-fns/locale/en-US';
 import { ko } from 'date-fns/locale/ko';
+import { getPlans } from '@/lib/utils/supabaseApi';
+import { useEffect, useState } from 'react';
+import { CalendarEventType } from '@/types/plans';
 
 // 로케일(지역화) 설정
 const locales = {
@@ -19,11 +22,34 @@ const localizer = dateFnsLocalizer({
 });
 
 const MainCalendar = () => {
+  const [events, setEvents] = useState<CalendarEventType[]>([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const plans = await getPlans();
+
+        const formatted = plans.map(
+          (plan): CalendarEventType => ({
+            id: plan.plan_id,
+            title: plan.title,
+            start: new Date(plan.start_date),
+            end: new Date(plan.end_date),
+          })
+        );
+        setEvents(formatted);
+      } catch (error) {
+        console.error('일정 목록 가져오기 에러', error);
+      }
+    };
+    fetchEvents();
+  }, []);
+
   return (
     <div>
       <Calendar
         localizer={localizer}
-        events={[]} // 여기에 이벤트 추가 가능
+        events={events} // 여기에 이벤트 추가 가능
         startAccessor='start'
         endAccessor='end'
         style={{ height: 500 }}
