665b109a36bbfcae57361377d14de230428232f7
DnJ0408 <oyj921205@gmail.com>
2025-04-05 01:17:07 +0900

    refactor:#3 공식 문서에 기반한 QueryClient 생성

---
+++ src/app/providers.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { isServer, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export const makeQueryClient = () => {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  })
+}
+
+let browserQueryClient: QueryClient | undefined = undefined
+
+const getQueryClient = () => {
+  if (isServer) {
+    return makeQueryClient()
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient()
+      return browserQueryClient;
+  }
+}
+
+export const Providers = ({children}: {children: React.ReactNode}) => {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  );
+}
+
+export default Providers;
