c5dbf5ceb483cf0bbd9b3e45b54f7507bff5600b
JinChaeKim <rlawlsco88@gmail.com>
2025-04-04 10:03:58 +0900

    feat:#5 라우터명 수정 및 기본 캘린더 출력

---
+++ src/app/(pages)/calendar/page.tsx
@@ -0,0 +1,10 @@
+import MainCalendar from '@/components/commons/calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+export default function Home() {
+  return (
+    <div>
+      <MainCalendar />
+    </div>
+  );
+}
---
+++ src/components/commons/calendar.tsx
@@ -0,0 +1,35 @@
+'use client';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { format, parse, startOfWeek, getDay } from 'date-fns';
+import { enUS } from 'date-fns/locale/en-US';
+import { ko } from 'date-fns/locale/ko';
+
+// 로케일(지역화) 설정
+const locales = {
+  'en-US': enUS, //미국-영어 : 미국식 날짜
+  'ko-KR': ko, //한국-한글 : 한국식 날짜
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const MainCalendar = () => {
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={[]} // 여기에 이벤트 추가 가능
+        startAccessor='start'
+        endAccessor='end'
+        style={{ height: 500 }}
+      />
+    </div>
+  );
+};
+
+export default MainCalendar;
