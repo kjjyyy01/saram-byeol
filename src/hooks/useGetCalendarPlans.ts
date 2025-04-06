a474dd9a0bf0f9d2e5347ae7535db417eb2752a7
JinChaeKim <rlawlsco88@gmail.com>
2025-04-06 23:17:50 +0900

    refactor:#5 초기값 삭제 (빈배열을 캐싱하는 문제)

--- src/hooks/useGetCalendarPlans.ts
+++ src/hooks/useGetCalendarPlans.ts
@@ -8,6 +8,7 @@ export const useGetCalendarPlans = () => {
     queryKey: [QUERY_KEY.CALENDAR_PLANS],
     queryFn: async () => {
       const plans: PlansType[] = await getPlans(); // supabase에서 plans 데이터 가져오기
+
       // 캘린더에 적용되도록 데이터 가공하기
       const events: CalendarEventType[] = plans.map((plan) => ({
         id: plan.plan_id,
@@ -17,7 +18,6 @@ export const useGetCalendarPlans = () => {
       }));
       return events;
     },
-    initialData: [], // undefined 아님 명시
     staleTime: 60 * 1000, // 1분
   });
 };
