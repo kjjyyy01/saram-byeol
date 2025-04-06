4c015563a24154230cb70697a13ea5735dd01e34
JinChaeKim <rlawlsco88@gmail.com>
2025-04-06 22:04:03 +0900

    feat:#5 커스텀 훅 : plans 데이터 가져오기 및 가공

---
+++ src/hooks/useGetCalendarPlans.ts
@@ -0,0 +1,22 @@
+import { getPlans } from '@/app/api/supabase/service';
+import { QUERY_KEY } from '@/constants/queryKey';
+import { CalendarEventType, PlansType } from '@/types/plans';
+import { useQuery } from '@tanstack/react-query';
+
+export const useGetCalendarPlans = () => {
+  return useQuery<CalendarEventType[]>({
+    queryKey: [QUERY_KEY.CALENDAR_PLANS],
+    queryFn: async () => {
+      const plans: PlansType[] = await getPlans(); //supabase에서 plans 데이터 가져오기
+      //캘린더에 적용되도록 데이터 가공하기
+      const events: CalendarEventType[] = plans.map((plan) => ({
+        id: plan.plan_id,
+        title: plan.title,
+        start: new Date(plan.start_date),
+        end: new Date(plan.end_date),
+      }));
+      return events;
+    },
+    initialData: [], // undefined 아님 명시
+  });
+};
