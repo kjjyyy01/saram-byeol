538b900121028798f757552064d22a78506747b7
lee kiri <mango.man.e.ya@gmail.com>
2025-04-07 12:38:03 +0900

    feat#13 PlanForm 컴포넌트 input 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import PlanForm from '@/components/commons/PlanForm'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>Home
+      <div><PlanForm/></div>
+    </div>
   )
 }
 
--- src/components/commons/PlanForm.tsx
+++ src/components/commons/PlanForm.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Textarea } from '@/components/ui/textarea';
+import{Form } from"@/components/ui/form"
 
 const PlanForm = () => {
   return (
-    <div>PlanForm</div>
-  )
-}
+    <Form>
+      <div>
+        <Label htmlFor='title'>제목</Label>
+        <Input type='text' id='title' placeholder='제목' />
+      </div>
+      <div>
+        <Label htmlFor='dateInput'>날짜</Label>
+        <Input id='dateInput' />
+      </div>
+      <div>
+        <Label htmlFor='contacts'>내사람</Label>
+        <Input id='contacts' />
+      </div>
+      <div>
+        <Label htmlFor='detail'>내용</Label>
+        <Textarea id='detail' />
+      </div>
+    </Form>
+  );
+};
 
-export default PlanForm
+export default PlanForm;
