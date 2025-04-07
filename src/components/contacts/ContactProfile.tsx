1c3e4b20824dfab4aac2ca3f2bff3386e8920d99
siheon <rlatlgjs357@gmail.com>
2025-04-07 18:51:02 +0900

    feat:#7 친구 프로필 이미지 출력

--- src/components/contacts/ContactProfile.tsx
+++ src/components/contacts/ContactProfile.tsx
@@ -1,4 +1,5 @@
 import { ContactDetailType } from '@/types/contacts';
+import Image from 'next/image';
 
 interface Props {
   contact: ContactDetailType;
@@ -7,6 +8,22 @@ interface Props {
 const ContactProfile: React.FC<Props> = ({ contact }) => {
   return (
     <div className='mb-8'>
+      <div className='h-12 w-12 flex-shrink-0 overflow-hidden rounded-full bg-gray-200'>
+        {contact.contacts_profile_img ? (
+          <Image
+            src={contact.contacts_profile_img}
+            alt={contact.name}
+            width={48}
+            height={48}
+            className='h-full w-full object-cover'
+          />
+        ) : (
+          <div className='flex h-full w-full items-center justify-center text-lg font-bold text-gray-500'>
+            {contact.name.charAt(0)}
+          </div>
+        )}
+      </div>
+      
       <h1 className='mb-4 text-2xl font-bold'>{contact.name}</h1>
       <ul className='space-y-1'>
         <li>
