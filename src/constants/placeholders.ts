4b809e46d4ed75188502c6bdd717d9a7ab3e60d2
Jongyeon Kim <tyjysong0712@gmail.com>
2025-04-08 20:01:56 +0900

    feat:#18 비밀번호 확인 플레이스홀더 추가

--- src/constants/placeholders.ts
+++ src/constants/placeholders.ts
@@ -1,3 +1,4 @@
 export const PLACEHOLDER_EMAIL = 'sarambyeol@sarambyeol.com';
-export const PLACEHOLDER_PASSWORD = '비밀번호를 입력해주세요.';
 export const PLACEHOLDER_NICKNAME = '헤엄치는 수달';
+export const PLACEHOLDER_PASSWORD = '비밀번호를 입력해주세요.';
+export const PLACEHOLDER_PASSWORD_CHECK = '비밀번호를 다시 입력해주세요.';
