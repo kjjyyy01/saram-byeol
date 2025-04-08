2ba4f5228d66d2c4937e52653a1d79f9d30b15a1
Jongyeon Kim <tyjysong0712@gmail.com>
2025-04-08 11:36:09 +0900

    refactor:#18 경로 및 플레이스홀더 상수를 객체에서 개별 상수로 변경

--- src/constants/placeholders.ts
+++ src/constants/placeholders.ts
@@ -1,11 +1,3 @@
-export const PLACEHOLDER = {
-  email: {
-    message: 'sarambyeol@sarambyeol.com',
-  },
-  password: {
-    message: '비밀번호를 입력해주세요.',
-  },
-  nickname: {
-    message: '헤엄치는 수달',
-  },
-};
+export const PLACEHOLDER_EMAIL = 'sarambyeol@sarambyeol.com';
+export const PLACEHOLDER_PASSWORD = '비밀번호를 입력해주세요.';
+export const PLACEHOLDER_NICKNAME = '헤엄치는 수달';
