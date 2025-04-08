b21b04998a9232fbc557ca0a7a63ec363ca44931
lee kiri <mango.man.e.ya@gmail.com>
2025-04-08 16:59:52 +0900

    feat#13 PlanForm 컴포넌트에서 새로운 스키마 및 유틸리티 함수 추가, ContactsField에 userId props 전달

--- src/components/plans/PlanForm.tsx
+++ src/components/plans/PlanForm.tsx
@@ -2,28 +2,17 @@
 
 import { FormProvider, useForm } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
-import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import TitleField from './TitleField';
 import DateInputField from './DateInputField';
 import ContactsField from './ContactsField';
 import DetailField from './DetailField';
+import useMutateInsertNewPlan from '@/hooks/mutations/useMutateInsertNewPlan';
+import { PlanFormType, PlansSchema } from '@/lib/schemas/plansSchema';
+import { mappingFormData } from '@/lib/planFormUtils';
 
-const PlansSchema = z.object({
-  title: z.string().min(1, {
-    message: '제목을 입력해주세요.',
-  }),
-  dateInput: z.object({
-    from: z.date(),
-    to: z.date().optional(),
-  }),
-  contacts: z.string(),
-  detail: z.string().optional(),
-});
+const TEST_USER_ID = 'a27fc897-4216-4863-9e7b-f8868a8369ff';
 
-//타입정의의 선언방식은 interface로 컨벤션되어있지만, interface는 조드가 지원해주는 자동 타입추론을 사용할 수 없습니다.
-//조드의 타입추론을 쓰면 스키마와 타입이 항상 동기화되는 장점이 있어서, 예외적으로 type을 사용한 타입선언을 했습니다.
-type PlanFormType = z.infer<typeof PlansSchema>;
 const PlanForm = () => {
   const form = useForm<PlanFormType>({
     resolver: zodResolver(PlansSchema),
@@ -39,10 +28,19 @@ const PlanForm = () => {
     },
   });
 
+  // mutate함수 호출
+  const { mutate: insertNewPlan } = useMutateInsertNewPlan();
+
   const planSubmitHandler = (data: PlanFormType) => {
-    //입력값 확인을 위한 임시 코드
-    alert(JSON.stringify(data));
-    form.reset();
+    const formData = mappingFormData(data);
+    insertNewPlan(
+      { user_id: TEST_USER_ID, ...formData },
+      {
+        onSuccess: () => {
+          form.reset();
+        },
+      }
+    );
   };
 
   return (
@@ -50,7 +48,7 @@ const PlanForm = () => {
       <form onSubmit={form.handleSubmit(planSubmitHandler)}>
         <TitleField />
         <DateInputField />
-        <ContactsField />
+        <ContactsField userId={TEST_USER_ID} />
         <DetailField />
         <Button type='submit' disabled={form.formState.isSubmitting}>
           약속 저장
---
+++ src/lib/planFormUtils.ts
@@ -0,0 +1,22 @@
+import { add } from 'date-fns';
+import { PlanFormType } from './schemas/plansSchema';
+
+//날짜에 1시간 씩 추가
+export const addTimeHour = (date: Date) => {
+  return add(date, { hours: 1 });
+};
+
+//종료일이 없으면 시작일을 종료일에 대입
+export const getStartAndEndDate = (from: Date, to?: Date) => {
+  const start_date = addTimeHour(from);
+  const end_date = to ? addTimeHour(to) : start_date;
+  return { start_date, end_date };
+};
+
+//입력 데이터 구조분해 할당
+export const mappingFormData = (data: PlanFormType) => {
+  const { title, detail, contacts, dateInput } = data;
+  const { start_date, end_date } = getStartAndEndDate(dateInput.from, dateInput.to);
+
+  return { title, detail, contacts_id: contacts, start_date, end_date };
+};
