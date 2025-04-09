1b8d6bce0cb85c2d9bf96a9ad5c28166a5dfb09c
lee kiri <mango.man.e.ya@gmail.com>
2025-04-09 16:50:32 +0900

    chore#22 카카오 API 호출을 위한 환경 변수 추가

--- src/constants/env.constant.ts
+++ src/constants/env.constant.ts
@@ -1,6 +1,10 @@
 const ENV = {
   SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || '',
   SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
+
+  //카카오 로컬 api호출용
+  KAKAO_KEY: process.env.KAKAO_REST_API_KEY || '',
+  KAKAO_URL: process.env.KAKAO_BASE_URL || '',
 };
 
 export default ENV;
