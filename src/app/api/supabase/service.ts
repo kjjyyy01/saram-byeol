0360d06beccb85072fe1d826c4d2a76f0a14edfc
siheon <rlatlgjs357@gmail.com>
2025-04-09 16:52:54 +0900

    fix:#24 pull dev

--- src/app/api/supabase/service.ts
+++ src/app/api/supabase/service.ts
@@ -2,7 +2,7 @@ import { ContactItemType, ContactWithPlansDetailType } from '@/types/contacts';
 import { supabase } from '@/app/api/supabase/client';
 import { SignInFormType } from '@/app/(pages)/signin/page';
 import { SignUpFormType } from '@/app/(pages)/signup/page';
-import { PlansType } from '@/types/plans';
+import { InsertNewPlansType, PlansType } from '@/types/plans';
 
 // contacts 데이터 가져오기
 export const getContacts = async (userId: string): Promise<ContactItemType[]> => {
