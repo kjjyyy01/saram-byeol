83baf8dc2aeaadffa9097d5a6dbb0cb4e332971a
JinChaeKim <rlawlsco88@gmail.com>
2025-04-10 17:09:59 +0900

    feat:#26 툴바에 버튼 추가(다가오는 일정, 일정 추가)

--- src/components/calendar/CustomToolbar.tsx
+++ src/components/calendar/CustomToolbar.tsx
@@ -2,6 +2,8 @@ import { CalendarEventType } from '@/types/plans';
 import { format } from 'date-fns';
 import { ko } from 'date-fns/locale';
 import { ToolbarProps } from 'react-big-calendar';
+import ComingPlans from '@/components/calendar/ComingPlans';
+import AddPlans from '@/components/calendar/AddPlans';
 
 //캘린더와 타입 맞춤(id 유실로 인한 오류)
 const CustomToolbar = ({ date, onNavigate }: ToolbarProps<CalendarEventType>) => {
@@ -12,6 +14,11 @@ const CustomToolbar = ({ date, onNavigate }: ToolbarProps<CalendarEventType>) =>
       <span>{customLabel}</span>
       <button onClick={() => onNavigate('NEXT')}>➡️</button>
       <button onClick={() => onNavigate('TODAY')}>Today</button>
+      <section>
+        {/* 검색창 추가 */}
+        <ComingPlans />
+        <AddPlans />
+      </section>
     </div>
   );
 };
