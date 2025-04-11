bcc9ae82945eed4dab59ec44b9a68421db699ee2
JinChaeKim <rlawlsco88@gmail.com>
2025-04-11 11:27:14 +0900

    refactor:#26 로딩 및 에러 처리

--- src/components/calendar/CalendarPopOver.tsx
+++ src/components/calendar/CalendarPopOver.tsx
@@ -7,7 +7,7 @@ import { TEST_USER_ID } from '@/components/contacts/ContactList';
 import { FormProvider, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '@/components/ui/button';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { Input } from '@/components/ui/input';
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 import ContactsField from '@/components//plans/ContactsField';
@@ -50,42 +50,53 @@ const CalendarPopOver = ({ open, onOpenChange, date }: Props) => {
   }, [date, open, form]);
 
   // mutate함수 호출
-  const { mutate: insertNewPlan } = useMutateInsertNewPlan();
+  const { mutate: insertNewPlan, isPending } = useMutateInsertNewPlan();
 
-  const planSubmitHandler = (data: PlanFormType) => {
-    const formData = mappingFormData(data);
-    insertNewPlan(
-      { user_id: TEST_USER_ID, ...formData },
-      {
-        onSuccess: () => {
-          form.reset();
-          toast.success('약속이 추가되었습니다!');
-        },
-      }
-    );
-  };
+  const planSubmitHandler = useCallback(
+    (data: PlanFormType) => {
+      const formData = mappingFormData(data);
+      insertNewPlan(
+        { user_id: TEST_USER_ID, ...formData },
+        {
+          onSuccess: () => {
+            form.reset();
+            toast.success('약속이 추가되었습니다.');
+          },
+          onError: () => {
+            toast.error('약속 저장에 실패했습니다.');
+          },
+        }
+      );
+    },
+    [insertNewPlan, form]
+  );
+
   return (
     <FormProvider {...form}>
       <Popover open={open} onOpenChange={onOpenChange}>
         <PopoverContent side='right' align='start' style={{ position: 'absolute', top: 200, left: 600 }} asChild>
           <form onSubmit={form.handleSubmit(planSubmitHandler)}>
-            <Form {...form}>
-              <FormField
-                control={form.control}
-                name='title'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input type='text' placeholder='제목' {...field} />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-              <DateInputField />
-              <ContactsField userId={TEST_USER_ID} />
-              <Button>옵션 더보기</Button>
-              <Button type='submit'>저장</Button>
-            </Form>
+            <fieldset disabled={isPending}>
+              <Form {...form}>
+                <FormField
+                  control={form.control}
+                  name='title'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input type='text' placeholder='제목' {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <DateInputField />
+                <ContactsField userId={TEST_USER_ID} />
+                <Button>옵션 더보기</Button>
+                <Button type='submit' disabled={isPending}>
+                  {isPending ? '저장 중...' : '저장'}
+                </Button>
+              </Form>
+            </fieldset>
           </form>
         </PopoverContent>
       </Popover>
