7086417bdbf7dd7f590e29cc69da1f8043189bd0
Jongyeon Kim <tyjysong0712@gmail.com>
2025-04-14 14:31:09 +0900

    feat:#39 로그인 및 회원가입 페이지 1차시안 UI 반영

--- src/app/(pages)/signIn/page.tsx
+++ src/app/(pages)/signIn/page.tsx
@@ -10,6 +10,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { signInSchema } from '@/lib/schemas/signinSchema';
 import { z } from 'zod';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export type SignInFormType = z.infer<typeof signInSchema>;
 
@@ -48,34 +49,110 @@ const SignIn = () => {
     if (error) alert('로그인 중 오류가 발생했습니다. 새로고침 후 다시 로그인해주세요.');
   };
 
+  const alreadyService = () => {
+    alert('아직 준비중인 기능입니다.');
+  };
+
   return (
-    <form onSubmit={handleSubmit(onSignInHandler)}>
-      <div className='flex flex-col'>
-        <section>
-          <label htmlFor='email'>아이디</label>
-          <input type='email' id='email' placeholder={PLACEHOLDER_EMAIL} {...register('email')} />
+    <div className='flex h-[100vh] w-[100vw] items-center justify-center overflow-hidden'>
+      <section className='flex w-1/2 flex-col items-center justify-center'>
+        <h1 className="mb-10 text-center font-['Pretendard'] text-[28px] font-bold text-[#06F]">사람, 별 로그인</h1>
+        <section className='mb-8'>
+          <form onSubmit={handleSubmit(onSignInHandler)} className='flex flex-col gap-8'>
+            <div className='flex flex-col'>
+              <div className='flex flex-col justify-start'>
+                <label
+                  className={`self-stretch font-['Pretendard'] text-sm font-bold leading-[150%] ${formState.errors.email ? `text-[#FF4242]` : `text-[#2B2B2B]`}`}
+                  htmlFor='email'
+                >
+                  아이디(이메일)
+                </label>
+                <input
+                  className={`flex-1 items-center gap-2 self-stretch rounded-lg border p-4 ${formState.errors.email ? `border-[#FF4242] focus:outline-none` : `border-[#B9B9B9] p-4`}`}
+                  type='email'
+                  id='email'
+                  placeholder={PLACEHOLDER_EMAIL}
+                  {...register('email')}
+                />
+              </div>
+              {formState.errors.email && (
+                <span className='self-stretch font-[Pretendard] text-sm font-normal not-italic leading-[150%] text-[#FF4242]'>
+                  {formState.errors.email.message}
+                </span>
+              )}
+            </div>
+            <div className='flex flex-col'>
+              <div className='flex flex-col justify-start'>
+                <label
+                  className={`self-stretch font-['Pretendard'] text-sm font-bold leading-[150%] ${formState.errors.password ? `text-[#FF4242]` : `text-[#2B2B2B]`}`}
+                  htmlFor='password'
+                >
+                  비밀번호
+                </label>
+                <input
+                  className={`flex-1 items-center gap-2 self-stretch rounded-lg border p-4 ${formState.errors.password ? `border-[#FF4242] focus:outline-none` : `border-[#B9B9B9] p-4`}`}
+                  type='password'
+                  id='password'
+                  placeholder={PLACEHOLDER_PASSWORD}
+                  {...register('password')}
+                />
+              </div>
+              {formState.errors.password && (
+                <span className='self-stretch font-[Pretendard] text-sm font-normal not-italic leading-[150%] text-[#FF4242]'>
+                  {formState.errors.password.message}
+                </span>
+              )}
+            </div>
+            <div className='flex justify-between'>
+              <label htmlFor='saveId'>
+                <input type='checkbox' id='saveId' onClick={alreadyService} className='mr-1' />
+                로그인 정보 저장
+              </label>
+              <button type='button' onClick={alreadyService}>
+                비밀번호를 잊어버리셨습니까?
+              </button>
+            </div>
+            <button
+              type='submit'
+              className="duration-600 w-[456px] justify-start rounded-lg bg-[#06F] px-6 py-4 font-['Pretendard'] text-base font-bold not-italic leading-[135%] text-white transition hover:bg-[#EEE] hover:text-[#06F]"
+            >
+              로그인
+            </button>
+          </form>
         </section>
-        {formState.errors.email && <span>{formState.errors.email.message}</span>}
-      </div>
 
-      <div className='flex flex-col'>
-        <section>
-          <label htmlFor='password'>비밀번호</label>
-          <input type='password' id='password' placeholder={PLACEHOLDER_PASSWORD} {...register('password')} />
+        <section className='flex flex-col items-center justify-center'>
+          <div className='mb-[42px] flex'>
+            <p className='mr-1'>아이디가 없으신가요?</p>
+            <Link href={SIGNUP} className='text-[#06F]'>
+              회원가입
+            </Link>
+          </div>
+          <div className='mb-[42px] flex items-center gap-1'>
+            <div className='w-[150px] outline outline-1 outline-stone-300' />
+            <p className='text-base font-bold leading-normal text-zinc-800'>SNS계정으로 간편로그인</p>
+            <div className='h-0 w-[150px] outline outline-1 outline-stone-300' />
+          </div>
+          <div className='flex items-center justify-center gap-[78px]'>
+            <button
+              type='button'
+              onClick={googleSignin}
+              className='flex h-[45px] w-[300px] items-center justify-center gap-[10px] rounded-[6px] border-2 border-[#F5F5F5] font-[Pretendard] text-sm font-semibold text-[#595959]'
+            >
+              <Image src='/google_login_img.png' alt='google login img' width={22} height={22} />
+              구글계정으로 로그인
+            </button>
+            <button type='button' onClick={kakaoSignin}>
+              <Image src='/kakao_login_img.png' alt='kakao login img' width={300} height={45} />
+            </button>
+          </div>
         </section>
-        {formState.errors.password && <span>{formState.errors.password.message}</span>}
-      </div>
+      </section>
 
-      <button type='submit'>로그인</button>
-      <p>아이디가 없으신가요?</p>
-      <Link href={SIGNUP}>회원가입</Link>
-      <button type='button' onClick={googleSignin}>
-        구글 로그인
-      </button>
-      <button type='button' onClick={kakaoSignin}>
-        카카오 로그인
-      </button>
-    </form>
+      <section className='relative h-full w-1/2'>
+        <Image src={'/saram-byeol_img.avif'} alt='metaphor image' fill className='object-contain' />
+      </section>
+    </div>
   );
 };
 
--- src/app/(pages)/signUp/page.tsx
+++ src/app/(pages)/signUp/page.tsx
@@ -21,6 +21,7 @@ import { PEOPLE } from '@/constants/paths';
 import { useState } from 'react';
 import { signUpSchema } from '@/lib/schemas/signupSchema';
 import { z } from 'zod';
+import Image from 'next/image';
 
 export type SignUpFormType = z.infer<typeof signUpSchema>;
 
@@ -118,68 +119,167 @@ const SignUp = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSignUpHandler)}>
-      <div className='flex flex-col'>
-        <section>
-          <label htmlFor='nickname'>닉네임</label>
-          <input type='text' id='nickname' maxLength={8} placeholder={PLACEHOLDER_NICKNAME} {...register('nickname')} />
-          <button type='button' onClick={NicknameDuplicateTestHandler}>
-            중복 검사
-          </button>
-        </section>
-        {formState.errors.nickname ? (
-          <span>{formState.errors.nickname.message}</span>
-        ) : (
-          <span>2자 이상 8자 이하로 입력해주세요.</span>
-        )}
-      </div>
+    <div className='flex h-[100vh] w-[100vw] items-center justify-center overflow-hidden'>
+      <section className='flex w-1/2 flex-col items-center justify-center'>
+        <h1 className="mb-10 text-center font-['Pretendard'] text-[28px] font-bold text-[#06F]">사람, 별 회원가입</h1>
+        <section className='mb-8'>
+          <form onSubmit={handleSubmit(onSignUpHandler)} className='flex flex-col gap-8'>
+            <div className='flex flex-col'>
+              <div className='flex flex-col justify-start'>
+                <label
+                  className={`self-stretch font-['Pretendard'] text-sm font-bold leading-[150%] ${formState.errors.nickname ? `text-[#FF4242]` : `text-[#2B2B2B]`}`}
+                >
+                  이름(닉네임)
+                </label>
+                <div className='flex flex-row gap-6'>
+                  <input
+                    className={`flex-1 items-center gap-2 self-stretch rounded-lg border p-4 ${formState.errors.nickname ? `border-[#FF4242] focus:outline-none` : `border-[#B9B9B9] p-4`}`}
+                    type='text'
+                    id='nickname'
+                    maxLength={8}
+                    placeholder={PLACEHOLDER_NICKNAME}
+                    {...register('nickname')}
+                  />
+                  <button
+                    type='button'
+                    className='duration-300" rounded-[8px] border border-gray-600 bg-white px-6 py-4 transition hover:bg-[#06F] hover:text-white'
+                    onClick={NicknameDuplicateTestHandler}
+                  >
+                    중복 검사
+                  </button>
+                </div>
+              </div>
+              {formState.errors.nickname ? (
+                <span className='self-stretch font-[Pretendard] text-sm font-normal not-italic leading-[150%] text-[#FF4242]'>
+                  {formState.errors.nickname.message}
+                </span>
+              ) : (
+                <span className='self-stretch font-[Pretendard] font-normal not-italic leading-[150%] text-[#D0D0D0]'>
+                  2자 이상 8자 이하로 입력해주세요.
+                </span>
+              )}
+            </div>
 
-      <div className='flex flex-col'>
-        <section>
-          <label htmlFor='email'>아이디(이메일)</label>
-          <input type='email' id='email' placeholder={PLACEHOLDER_EMAIL} {...register('email')} />
-          <button type='button' onClick={EmailDuplicateTestHandler}>
-            중복 검사
-          </button>
-        </section>
-        {formState.errors.email && <span>{formState.errors.email.message}</span>}
-      </div>
+            <div className='flex flex-col'>
+              <div className='flex flex-col justify-start'>
+                <label
+                  className={`self-stretch font-['Pretendard'] text-sm font-bold leading-[150%] ${formState.errors.email ? `text-[#FF4242]` : `text-[#2B2B2B]`}`}
+                >
+                  아이디(이메일)
+                </label>
+                <div className='flex flex-row gap-6'>
+                  <input
+                    className={`flex-1 items-center gap-2 self-stretch rounded-lg border p-4 ${formState.errors.email ? `border-[#FF4242] focus:outline-none` : `border-[#B9B9B9] p-4`}`}
+                    type='email'
+                    id='email'
+                    placeholder={PLACEHOLDER_EMAIL}
+                    {...register('email')}
+                  />
+                  <button
+                    type='button'
+                    className='duration-300" rounded-[8px] border border-gray-600 bg-white px-6 py-4 transition hover:bg-[#06F] hover:text-white'
+                    onClick={EmailDuplicateTestHandler}
+                  >
+                    중복 검사
+                  </button>
+                </div>
+              </div>
+              {formState.errors.email ? (
+                <span className='self-stretch font-[Pretendard] text-sm font-normal not-italic leading-[150%] text-[#FF4242]'>
+                  {formState.errors.email.message}
+                </span>
+              ) : (
+                <span className='self-stretch font-[Pretendard] font-normal not-italic leading-[150%] text-[#D0D0D0]'>
+                  이메일 형식에 맞게 입력해주세요.
+                </span>
+              )}
+            </div>
 
-      <div className='flex flex-col'>
-        <section>
-          <label htmlFor='password'>비밀번호</label>
-          <input type='password' id='password' placeholder={PLACEHOLDER_PASSWORD} {...register('password')} />
-        </section>
-        {formState.errors.password ? (
-          <span>{formState.errors.password.message}</span>
-        ) : (
-          <span>특수문자(!@#$%^&*)를 1개 이상 포함하여 입력해주세요.</span>
-        )}
-      </div>
+            <div className='flex flex-col'>
+              <div className='flex flex-col justify-start'>
+                <label
+                  className={`self-stretch font-['Pretendard'] text-sm font-bold leading-[150%] ${formState.errors.password ? `text-[#FF4242]` : `text-[#2B2B2B]`}`}
+                >
+                  비밀번호
+                </label>
+                <input
+                  className={`flex-1 items-center gap-2 self-stretch rounded-lg border p-4 ${formState.errors.password ? `border-[#FF4242] focus:outline-none` : `border-[#B9B9B9] p-4`}`}
+                  type='password'
+                  id='password'
+                  placeholder={PLACEHOLDER_PASSWORD}
+                  {...register('password')}
+                />
+              </div>
+              {formState.errors.password ? (
+                <span className='self-stretch font-[Pretendard] text-sm font-normal not-italic leading-[150%] text-[#FF4242]'>
+                  {formState.errors.password.message}
+                </span>
+              ) : (
+                <span className='self-stretch font-[Pretendard] font-normal not-italic leading-[150%] text-[#D0D0D0]'>
+                  특수문자(!@#$%^&*)를 1개 이상 포함하여 입력해주세요.
+                </span>
+              )}
+            </div>
 
-      <div className='flex flex-col'>
-        <section>
-          <label htmlFor='passwordCheck'>비밀번호 확인</label>
-          <input
-            type='password'
-            id='passwordCheck'
-            placeholder={PLACEHOLDER_PASSWORD_CHECK}
-            {...register('passwordCheck')}
-          />
+            <div className='flex flex-col'>
+              <div className='flex flex-col justify-start'>
+                <label
+                  className={`self-stretch font-['Pretendard'] text-sm font-bold leading-[150%] ${formState.errors.passwordCheck ? `text-[#FF4242]` : `text-[#2B2B2B]`}`}
+                >
+                  비밀번호 확인
+                </label>
+                <input
+                  className={`flex-1 items-center gap-2 self-stretch rounded-lg border p-4 ${formState.errors.passwordCheck ? `border-[#FF4242] focus:outline-none` : `border-[#B9B9B9] p-4`}`}
+                  type='password'
+                  id='passwordCheck'
+                  placeholder={PLACEHOLDER_PASSWORD_CHECK}
+                  {...register('passwordCheck')}
+                />
+              </div>
+              {formState.errors.passwordCheck ? (
+                <span className='self-stretch font-[Pretendard] text-sm font-normal not-italic leading-[150%] text-[#FF4242]'>
+                  {formState.errors.passwordCheck.message}
+                </span>
+              ) : (
+                <span className='self-stretch font-[Pretendard] font-normal not-italic leading-[150%] text-[#D0D0D0]'>
+                  비밀번호를 한번 더 입력해주세요.
+                </span>
+              )}
+            </div>
+            <button
+              type='submit'
+              className="duration-600 w-[456px] justify-start rounded-lg bg-[#06F] px-6 py-4 font-['Pretendard'] text-base font-bold not-italic leading-[135%] text-white transition hover:bg-[#EEE] hover:text-[#06F]"
+            >
+              회원가입
+            </button>
+          </form>
         </section>
-        {formState.errors.passwordCheck && <span>{formState.errors.passwordCheck.message}</span>}
-      </div>
-
-      <button type='submit'>회원가입</button>
-      <section>
-        <button type='button' onClick={googleSignin}>
-          구글 로그인
-        </button>
-        <button type='button' onClick={kakaoSignin}>
-          카카오 로그인
-        </button>
+        <section className='flex flex-col items-center justify-center'>
+          <div className='mb-[42px] flex items-center gap-1'>
+            <div className='w-[150px] outline outline-1 outline-stone-300' />
+            <p className='text-base font-bold leading-normal text-zinc-800'>SNS계정으로 간편로그인</p>
+            <div className='h-0 w-[150px] outline outline-1 outline-stone-300' />
+          </div>
+          <div className='flex items-center justify-center gap-[78px]'>
+            <button
+              type='button'
+              onClick={googleSignin}
+              className='flex h-[45px] w-[300px] items-center justify-center gap-[10px] rounded-[6px] border-2 border-[#F5F5F5] font-[Pretendard] text-sm font-semibold text-[#595959]'
+            >
+              <Image src='/google_login_img.png' alt='google login' width={22} height={22} />
+              구글계정으로 로그인
+            </button>
+            <button type='button' onClick={kakaoSignin}>
+              <Image src='/kakao_login_img.png' alt='kakao login' width={300} height={45} />
+            </button>
+          </div>
+        </section>
       </section>
-    </form>
+
+      <section className='relative h-full w-1/2'>
+        <Image src={'/saram-byeol_img.avif'} alt='metaphor image' fill className='object-contain' />
+      </section>
+    </div>
   );
 };
 
