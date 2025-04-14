952d6c1523b69accd2f93607bb23b02520a7eec4
lee kiri <mango.man.e.ya@gmail.com>
2025-04-14 23:12:38 +0900

    fix#46 제목 입력 필드 placeholder 수정 - '제목'에서 '제목을 입력해주세요.'로 변경 및 최대 길이 20자에서 50자로 수정

--- src/components/plans/TitleField.tsx
+++ src/components/plans/TitleField.tsx
@@ -15,7 +15,7 @@ const TitleField = () => {
           <FormItem>
             <FormLabel>제목</FormLabel>
             <FormControl>
-              <Input type='text' placeholder='제목' {...field} maxLength={20}/>
+              <Input type='text' placeholder='제목을 입력해주세요.' {...field} maxLength={50}/>
             </FormControl>
             <FormMessage />
           </FormItem>
