58ae4eeace7b787d55e38ce57a774d751a322b75
lee kiri <mango.man.e.ya@gmail.com>
2025-04-14 16:15:07 +0900

    feat#40 메타데이터 추가 및 HTML 구조 수정

--- src/app/(pages)/(nav)/layout.tsx
+++ src/app/(pages)/(nav)/layout.tsx
@@ -1,14 +1,18 @@
 import LeftNavBar from '@/components/nav/LeftNavBar';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '사람 별',
+  description: '연락처 관리는 사람 별',
+};
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <body className='flex flex-row'>
-        <header className='flex items-center'>
-          <LeftNavBar />
-        </header>
-        <main className='w-full'>{children}</main>
-      </body>
-    </html>
+    <div className="flex flex-row" >
+      <header className='flex items-center'>
+        <LeftNavBar />
+      </header>
+      <main className='w-full'>{children}</main>
+    </div>
   );
 }
