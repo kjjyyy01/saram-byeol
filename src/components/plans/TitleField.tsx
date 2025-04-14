9505f1f9756570163bc4588ff7fd23edcd49fcba
lee kiri <mango.man.e.ya@gmail.com>
2025-04-15 00:51:47 +0900

    style#46 TitleField 컴포넌트의 구조 개선 및 스타일 추가

--- src/components/plans/TitleField.tsx
+++ src/components/plans/TitleField.tsx
@@ -3,6 +3,7 @@
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { useFormContext } from 'react-hook-form';
 import { Input } from '@/components/ui/input';
+import { TextAa } from '@phosphor-icons/react';
 
 const TitleField = () => {
   const { control } = useFormContext();
@@ -12,12 +13,23 @@ const TitleField = () => {
       name='title'
       render={({ field }) => {
         return (
-          <FormItem>
-            <FormLabel>제목</FormLabel>
-            <FormControl>
-              <Input type='text' placeholder='제목을 입력해주세요.' {...field} maxLength={50}/>
-            </FormControl>
-            <FormMessage />
+          <FormItem className='flex items-center justify-start gap-8'>
+            <FormLabel className='relative flex w-14 flex-shrink-0 flex-grow-0 flex-col items-center justify-center gap-1'>
+              <TextAa size={24} className='h-6 w-6 flex-shrink-0 flex-grow-0' />
+              <p className='text-center text-sm'>제목</p>
+            </FormLabel>
+            <div className='flex w-full flex-col'>
+              <FormControl>
+                <Input
+                  type='text'
+                  placeholder='제목을 입력해주세요.'
+                  {...field}
+                  maxLength={50}
+                  className={`border-grey-200 flex flex-1 items-center self-stretch rounded-lg border p-4 text-base leading-6`}
+                />
+              </FormControl>
+              <FormMessage className='pl-1' />
+            </div>
           </FormItem>
         );
       }}
