a49bf04e03b09b39f963839fd373f411ce0b5002
JinChaeKim <rlawlsco88@gmail.com>
2025-04-14 21:16:24 +0900

    feat:#44 상단 툴바 UI 적용

--- src/components/calendar/CustomToolbar.tsx
+++ src/components/calendar/CustomToolbar.tsx
@@ -2,36 +2,20 @@ import { CalendarEventType } from '@/types/plans';
 import { format } from 'date-fns';
 import { ko } from 'date-fns/locale';
 import { ToolbarProps } from 'react-big-calendar';
-import AddPlans from '@/components/calendar/AddPlans';
-import UpcomingPlans from '@/components/calendar/UpcomingPlans';
-import { ArrowLeft, ArrowRight } from '@phosphor-icons/react';
+import UpcomingPlanButton from './UpcomingPlanButton';
+import AddPlanButton from '@/components/calendar/AddPlanButton';
+import MoveMonthButton from './MoveMonthButton';
 
 //캘린더와 타입 맞춤(id 유실로 인한 오류)
 const CustomToolbar = ({ date, onNavigate }: ToolbarProps<CalendarEventType>) => {
   const customLabel = format(date, 'yyyy MMMM', { locale: ko });
   return (
-    <div className='flex'>
-      <section className='flex h-[40px] w-[137px] place-content-center items-center rounded-[8px] border border-gray-300'>
-        <button
-          onClick={() => onNavigate('PREV')}
-          className='flex h-full flex-1 items-center justify-center border-r border-gray-300'
-        >
-          <ArrowLeft size={20} />
-        </button>
-        <button
-          onClick={() => onNavigate('NEXT')}
-          className='flex h-full flex-1 items-center justify-center border-r border-gray-300'
-        >
-          <ArrowRight size={20} />
-        </button>
-        <button onClick={() => onNavigate('TODAY')} className='flex flex-1 items-center justify-center'>
-          오늘
-        </button>
-      </section>
+    <div className='mt-[18px] flex'>
+      <MoveMonthButton onNavigate={onNavigate} />
       <span className='ml-[24px] text-[28px] font-bold'>{customLabel}</span>
-      <section className='flex'>
-        <UpcomingPlans />
-        <AddPlans />
+      <section className='mb-[12.5px] ml-auto mr-[9px] flex'>
+        <UpcomingPlanButton />
+        <AddPlanButton />
       </section>
     </div>
   );
