6d900683ca0d97ef5f892a93bf060bd050cd7d76
lee kiri <mango.man.e.ya@gmail.com>
2025-04-14 22:54:52 +0900

    fix#46 코드 스타일 개선 및 priority 필드 추가

--- src/lib/planFormUtils.ts
+++ src/lib/planFormUtils.ts
@@ -1,27 +1,26 @@
 import { PlanFormType } from './schemas/plansSchema';
 
-
-export const getTimeToString = (date: Date)=>{
+export const getTimeToString = (date: Date) => {
   const yyyy = date.getFullYear().toString();
   const mm = date.getMonth() + 1;
   const dd = date.getDate();
-  return {yyyy, mm, dd}
-}
+  return { yyyy, mm, dd };
+};
 //시작일과 종료일 시간 추가 처리
 export const getStartAndEndDate = (from: Date, to?: Date) => {
-  const {yyyy:fromY, mm:fromM, dd:fromD } = getTimeToString(from)
-  const {yyyy:toY, mm:toM, dd:toD } = to? getTimeToString(to) : getTimeToString(from)
+  const { yyyy: fromY, mm: fromM, dd: fromD } = getTimeToString(from);
+  const { yyyy: toY, mm: toM, dd: toD } = to ? getTimeToString(to) : getTimeToString(from);
   const start_date = `${fromY}-${fromM}-${fromD} 00:00:00`;
-  const end_date = `${toY}-${toM}-${toD} 23:59:59`
+  const end_date = `${toY}-${toM}-${toD} 23:59:59`;
 
   return { start_date, end_date };
 };
 
 //입력 데이터 구조분해 할당
 export const mappingFormData = (data: PlanFormType) => {
-  const { title, detail, contacts, dateInput, location } = data;
+  const { title, detail, contacts, dateInput, location, priority } = data;
   const { start_date, end_date } = getStartAndEndDate(dateInput.from, dateInput.to);
-  return { title, detail, contacts_id: contacts, start_date,end_date, location };
+  return { title, detail, contacts_id: contacts, start_date, end_date, location, priority };
 };
 
 //검색없이 인풋값만 있을 때 데이터 처리
@@ -37,5 +36,4 @@ export const inputToPlace = (input: string) => {
   };
   return place;
 };
-// 나중에 시간정보가 추가되면, 시간정보가 있을 때는 날짜에서 시간을 빼고 저장하고..
-//시간 값은 별도 테이블에 담기...
+
