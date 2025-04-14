42111d14f0f34431b065a94c385f3f106794e263
JinChaeKim <rlawlsco88@gmail.com>
2025-04-14 16:44:52 +0900

    style:#44 달 이동 버튼 UI 수정 중

--- src/components/calendar/CustomToolbar.tsx
+++ src/components/calendar/CustomToolbar.tsx
@@ -4,20 +4,30 @@ import { ko } from 'date-fns/locale';
 import { ToolbarProps } from 'react-big-calendar';
 import AddPlans from '@/components/calendar/AddPlans';
 import UpcomingPlans from '@/components/calendar/UpcomingPlans';
+import { ArrowLeft, ArrowRight } from '@phosphor-icons/react';
 
 //캘린더와 타입 맞춤(id 유실로 인한 오류)
 const CustomToolbar = ({ date, onNavigate }: ToolbarProps<CalendarEventType>) => {
   const customLabel = format(date, 'yyyy MMMM', { locale: ko });
   return (
     <div>
-      <section>
-        <button onClick={() => onNavigate('PREV')}>⬅️</button>
-        <button onClick={() => onNavigate('NEXT')}>➡️</button>
-        <button onClick={() => onNavigate('TODAY')}>오늘</button>
+      <section className='flex h-[40px] w-[137px] place-content-center items-center rounded-xl border border-gray-300'>
+        <ArrowLeft
+          size={20}
+          onClick={() => onNavigate('PREV')}
+          className='flex h-full flex-1 items-center justify-center border-r border-gray-300'
+        ></ArrowLeft>
+        <ArrowRight
+          size={20}
+          onClick={() => onNavigate('NEXT')}
+          className='flex h-full flex-1 items-center justify-center border-r border-gray-300'
+        ></ArrowRight>
+        <button onClick={() => onNavigate('TODAY')} className='flex flex-1 items-center justify-center'>
+          오늘
+        </button>
       </section>
       <span>{customLabel}</span>
       <section>
-        {/* 검색창 추가 */}
         <UpcomingPlans />
         <AddPlans />
       </section>
