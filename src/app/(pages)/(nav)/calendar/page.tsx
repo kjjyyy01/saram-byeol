7f298c15ca841848487c71db218796fa899723d0
JinChaeKim <rlawlsco88@gmail.com>
2025-04-15 20:51:14 +0900

    refactor:#61 로그인 상태 적용

--- src/app/(pages)/(nav)/calendar/page.tsx
+++ src/app/(pages)/(nav)/calendar/page.tsx
@@ -1,23 +1,31 @@
+'use client';
 import MainCalendar from '@/components/calendar/MainCalendar';
 import UpcomingPlans from '@/components/schedule/UpcomingPlans';
-import { Metadata } from 'next';
+import { SIGNIN } from '@/constants/paths';
+import { useAuthStore } from '@/store/zustand/store';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-export const metadata: Metadata = {
-  title: '사람 별 캘린더',
-  description: '약속을 모아볼 수 있는 캘린더 페이지입니다.',
-};
-
 export default function Calendar() {
-  const TEST_USER_ID = 'a27fc897-4216-4863-9e7b-f8868a8369ff';
+  const router = useRouter();
+  const user = useAuthStore((state) => state.user);
+  const isSignIn = useAuthStore((state) => state.isSignIn);
+
+  useEffect(() => {
+    if (!isSignIn) {
+      router.replace(SIGNIN);
+    }
+  }, [isSignIn, router]);
+
+  if (!isSignIn) return null;
+
   return (
-    <div className="flex flex-col md:flex-row gap-4 p-4">
-      <div className="md:flex-grow">
+    <div className='flex flex-col gap-4 p-4 md:flex-row'>
+      <div className='md:flex-grow'>
         <MainCalendar />
       </div>
-      <div className="md:w-auto flex-shrink-0">
-        <UpcomingPlans userId={TEST_USER_ID} />
-      </div>
+      <div className='flex-shrink-0 md:w-auto'>{user?.id && <UpcomingPlans userId={user.id} />}</div>
     </div>
   );
 }
