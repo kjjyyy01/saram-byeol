609c044bc0b84eae2bef0e1913318de1c5ec6235
JinChaeKim <rlawlsco88@gmail.com>
2025-04-18 12:25:30 +0900

    refactor:#109 '다가오는 약속' 버튼 숫자 state 초기값 수정

--- src/components/calendar/UpcomingPlanButton.tsx
+++ src/components/calendar/UpcomingPlanButton.tsx
@@ -4,13 +4,13 @@ import React, { useEffect, useState } from 'react';
 
 const UpcomingPlanButton = ({ onClick }: { onClick: () => void }) => {
   const user = useAuthStore((state) => state.user);
-  const [upcomingCount, setUpcomingCount] = useState(0);
+  const [upcomingCount, setUpcomingCount] = useState<number | null>(null);
 
   useEffect(() => {
     const fetchPlans = async () => {
       if (user?.id) {
         const upcomingPlans = await getUserPlans(user.id);
-        setUpcomingCount(upcomingPlans.length);
+        setUpcomingCount(upcomingPlans.length); // 데이터 도착한 후에만 업데이트
       }
     };
     fetchPlans();
@@ -20,7 +20,9 @@ const UpcomingPlanButton = ({ onClick }: { onClick: () => void }) => {
     <div>
       <button onClick={onClick} className='mr-[24px] rounded-md border-[1px] px-[20px] py-[12px] text-[14px] font-bold'>
         다가오는 약속
-        <span className='m-1 rounded-full bg-black px-1 text-white'>{upcomingCount}</span>
+        <span className='m-1 rounded-full bg-black px-1 text-white'>
+          {upcomingCount !== null ? upcomingCount : '-'}
+        </span>
       </button>
     </div>
   );
