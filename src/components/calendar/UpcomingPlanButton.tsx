71bc69f89390d44d13b8b2620820a514a8ab811e
JinChaeKim <rlawlsco88@gmail.com>
2025-04-18 11:18:16 +0900

    feat:#109 다가오는 약속 버튼에 다가오는 약속 개수 나타내기

--- src/components/calendar/UpcomingPlanButton.tsx
+++ src/components/calendar/UpcomingPlanButton.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import { getUserPlans } from '@/app/api/supabase/service';
+import { useAuthStore } from '@/store/zustand/store';
+import React, { useEffect, useState } from 'react';
 
 const UpcomingPlanButton = ({ onClick }: { onClick: () => void }) => {
+  const user = useAuthStore((state) => state.user);
+  const [upcomingCount, setUpcomingCount] = useState(0);
+
+  useEffect(() => {
+    const fetchPlans = async () => {
+      if (user?.id) {
+        const upcomingPlans = await getUserPlans(user.id);
+        setUpcomingCount(upcomingPlans.length);
+      }
+    };
+    fetchPlans();
+  }, [user?.id]);
+
   return (
     <div>
       <button onClick={onClick} className='mr-[24px] rounded-md border-[1px] px-[20px] py-[12px] text-[14px] font-bold'>
         다가오는 약속
-        {/* 다가오는 약속 갯수 추가 */}
+        <span className='m-1 rounded-full bg-black px-1 text-white'>{upcomingCount}</span>
       </button>
     </div>
   );
