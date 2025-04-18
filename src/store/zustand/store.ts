775ea622aa3d49baa2bc60dc76c2fbd135f0cad5
Jongyeon Kim <tyjysong0712@gmail.com>
2025-04-18 21:29:39 +0900

    refactor:#115 로그아웃 함수 이름 변경 및 토스트 메시지 수정

--- src/store/zustand/store.ts
+++ src/store/zustand/store.ts
@@ -1,5 +1,5 @@
 import { supabase } from '@/app/api/supabase/client';
-import { mutateSignOut } from '@/app/api/supabase/service';
+import { SignOutUser } from '@/app/api/supabase/service';
 import { User } from '@supabase/supabase-js';
 import { toast } from 'react-toastify';
 import { create } from 'zustand';
@@ -21,9 +21,8 @@ export const useAuthStore = create<AuthStateType>()(
       ...initialState,
       setUser: (user) => set({ user, isSignIn: true }),
       signOut: async () => {
-        await mutateSignOut();
+        await SignOutUser();
         set(initialState);
-        toast.success(`로그아웃되었습니다.`);
       },
     }),
     {
@@ -45,10 +44,11 @@ export const AuthStateChangeHandler = () => {
       // 토스트는 실제 로그인 액션 때만 띄우기
       if (event === 'SIGNED_IN' && !alreadySignIn) {
         localStorage.setItem('alreadySignIn', 'true');
-        toast.success(`로그인되었습니다. '내 사람' 페이지로 이동합니다.`);
+        toast.success(`로그인되었습니다. '내 사람'페이지로 이동합니다.`);
       }
     } else if (event === 'SIGNED_OUT') {
       localStorage.removeItem('alreadySignIn');
+      toast.success(`로그아웃되었습니다.`);
     }
   });
 
