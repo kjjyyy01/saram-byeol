ecef694a20d4824fe0c693ee46741896c02647a8
lee kiri <mango.man.e.ya@gmail.com>
2025-04-20 01:54:55 +0900

    feat#117 useDemoStore 구현 및 상태 관리 기능 추가

---
+++ src/store/zustand/useDemoStore.ts
@@ -0,0 +1,101 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+import type { ContactType } from '@/types/contacts';
+import type { PlansType } from '@/types/plans';
+
+// 약속 타입
+// export interface PlansType {
+//   plan_id: string;
+//   user_id: string;
+//   contacts_id: string;
+//   title: string;
+//   detail: string;
+//   priority: string;
+//   start_date: string;
+//   end_date: string;
+//   location?: Partial<KakaoPlaceType> | null;
+//   colors: string;
+// }
+// 사용자 타입
+// export interface UserType {
+//     id: string;
+//     user_id: string;
+//     email: string;
+//     nickname: string;
+//     profile_img?: string;
+//     created_at: string;
+//     updated_at: string;
+//   }
+
+//   // 연락처 타입
+//   export interface ContactType {
+//     id: string;
+//     contacts_id: string;
+//     user_id: string;
+//     name: string;
+//     relationship_level: string;
+//     email: string;
+//     notes: string;
+//     phone: string;
+//     birth: string;
+//     contacts_profile_img?: string;
+//     is_pinned?: boolean;
+//   }
+
+interface DemoState {
+  contacts: ContactType[];
+  plans: PlansType[];
+
+  addContact: (contact: ContactType) => void;
+  updateContact: (contact: ContactType) => void;
+  deleteContact: (id: string) => void;
+
+  addPlan: (plan: PlansType) => void;
+  deletePlan: (id: string) => void;
+
+  clearAll: () => void;
+}
+
+const initialState = {
+  contacts: [],
+  plans: [],
+};
+//내사람 추가, 수정, 삭제
+//약속 추가, 삭제
+//제한된 기능 제공
+export const useDemoStore = create<DemoState>()(
+  persist(
+    (set) => ({
+      ...initialState,
+
+      addContact: (person) => set((state) => ({ contacts: [...state.contacts, person] })),
+
+      updateContact: (person) =>
+        set((state) => ({
+          contacts: state.contacts.map((p) => (p.id === person.id ? person : p)),
+        })),
+
+      deleteContact: (id) =>
+        set((state) => ({
+          contacts: state.contacts.filter((p) => p.id !== id),
+          plans: state.plans.filter((a) => a.plan_id !== id),
+        })),
+
+      addPlan: (plan) =>
+        set((state) => ({
+          plans: [...state.plans, plan],
+        })),
+
+      deletePlan: (id) =>
+        set((state) => ({
+          plans: state.plans.filter((a) => a.plan_id !== id),
+        })),
+
+      clearAll: () => set(initialState),
+    }),
+    {
+      name: 'demo-storage',
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
