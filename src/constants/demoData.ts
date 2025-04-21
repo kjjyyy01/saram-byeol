d8ccffb0bd42e56f49433471ddd7167113a526c6
lee kiri <mango.man.e.ya@gmail.com>
2025-04-21 19:46:50 +0900

    feat#117 demoUser를 constants에서 가져와 useDemoStore에 통합하여 코드 중복 제거

---
+++ src/constants/demoData.ts
@@ -0,0 +1,13 @@
+export const demoUser = {
+  user: {
+    app_metadata: { provider: 'email' },
+    id: '297e8f39-2421-4fff-866a-d3887dd6effe',
+    user_metadata: {
+      email: 'demo-user@mail.com',
+      nickname: '김데모',
+      name: '김데모',
+      profile_img:
+        'https://sogssbvoxwlfglnmwmge.supabase.co/storage/v1/object/sign/demo-storage/user-profile/sung-wang-g4DgCF90EM4-unsplash.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJkZW1vLXN0b3JhZ2UvdXNlci1wcm9maWxlL3N1bmctd2FuZy1nNERnQ0Y5MEVNNC11bnNwbGFzaC5qcGciLCJpYXQiOjE3NDUyMTQ2MjEsImV4cCI6MTc0NzgwNjYyMX0.zGaHcIc7PHPkjDoNs_NT8YFyBa2RFBMwA07iDbKqhH0',
+    },
+  },
+};
--- src/store/zustand/useDemoStore.ts
+++ src/store/zustand/useDemoStore.ts
@@ -3,11 +3,26 @@ import { createJSONStorage, persist } from 'zustand/middleware';
 import type { ContactType } from '@/types/contacts';
 import type { PlansType } from '@/types/plans';
 import { toast } from 'react-toastify';
+import { demoUser } from '@/constants/demoData';
+// import { User } from '@supabase/supabase-js';
+interface DemoUser {
+  user: {
+    app_metadata: { provider: string };
+    id: string;
+    user_metadata: {
+      email: string;
+      nickname: string;
+      name: string;
+      profile_img: string;
+    };
+  };
+}
 
 interface DemoState {
   isDemoUser: boolean;
   contacts: ContactType[];
   plans: PlansType[];
+  demoUser: DemoUser;
 
   setDemoUser: () => void;
   addContact: (person: ContactType) => void;
@@ -26,22 +41,9 @@ const initialState = {
   isDemoUser: false,
   contacts: [],
   plans: [],
+  demoUser,
 };
-const demoUser = {
-  user: {
-    app_metadata: { provider: 'email' },
-    id: '297e8f39-2421-4fff-866a-d3887dd6effe',
-    user_metadata: {
-      email: 'demo-user@mail.com',
-      nickname: '김데모',
-      profile_img:
-        'https://sogssbvoxwlfglnmwmge.supabase.co/storage/v1/object/sign/demo-storage/user-profile/sung-wang-g4DgCF90EM4-unsplash.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJkZW1vLXN0b3JhZ2UvdXNlci1wcm9maWxlL3N1bmctd2FuZy1nNERnQ0Y5MEVNNC11bnNwbGFzaC5qcGciLCJpYXQiOjE3NDUyMTQ2MjEsImV4cCI6MTc0NzgwNjYyMX0.zGaHcIc7PHPkjDoNs_NT8YFyBa2RFBMwA07iDbKqhH0',
-    },
-  },
-};
-//내사람 추가, 수정, 삭제
-//약속 추가, 삭제
-//제한된 기능 제공
+
 export const useDemoStore = create<DemoState>()(
   persist(
     (set) => ({
