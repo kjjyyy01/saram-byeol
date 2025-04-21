c017f666c62d9c6b11a3de9f8ba9fd8b6f052d7e
Jongyeon Kim <tyjysong0712@gmail.com>
2025-04-21 21:07:59 +0900

    style:#119 회원가입 페이지의 여백 및 텍스트 스타일 수정

--- src/app/(pages)/signUp/page.tsx
+++ src/app/(pages)/signUp/page.tsx
@@ -9,13 +9,14 @@ const SignUp = () => {
     <div className='flex h-[100vh] w-[100vw] items-center justify-center overflow-y-auto'>
       <section className='flex w-full flex-col items-center justify-center'>
         <h1 className='mb-10 text-center text-[28px] font-bold text-primary-500'>사람, 별 회원가입</h1>
-        <section className='mb-8'>
+        <section className='mb-[42px]'>
           <SignupForm />
         </section>
-        <section className='mt-10 flex flex-col items-center justify-center'>
+
+        <section className='flex flex-col items-center justify-center'>
           <div className='mb-[42px] flex items-center gap-1'>
             <div className='w-[150px] outline outline-1 outline-stone-300' />
-            <p className='text-nowrap text-base font-bold leading-normal text-zinc-800'>SNS계정으로 간편로그인</p>
+            <p className='text-nowrap font-bold leading-normal text-zinc-800'>SNS계정으로 간편로그인</p>
             <div className='h-0 w-[150px] outline outline-1 outline-stone-300' />
           </div>
           <SigninSocial />
