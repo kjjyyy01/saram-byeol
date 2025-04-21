6d820657d3db33d597b73eb8308993521e27f79c
lee kiri <mango.man.e.ya@gmail.com>
2025-04-21 14:22:19 +0900

    feat#117 프로필 이미지 기본값을 상수로 분리

--- src/constants/placeholders.ts
+++ src/constants/placeholders.ts
@@ -2,3 +2,4 @@ export const PLACEHOLDER_EMAIL = 'sarambyeol@sarambyeol.com';
 export const PLACEHOLDER_NICKNAME = '김수달';
 export const PLACEHOLDER_PASSWORD = '비밀번호를 입력해주세요.';
 export const PLACEHOLDER_PASSWORD_CHECK = '비밀번호를 입력해주세요.';
+export const PLACEHOLDER_PROFILE_IMG = 'http://img1.kakaocdn.net/thumb/R640x640.q70/?fname=http://t1.kakaocdn.net/account_images/default_profile.jpeg'
