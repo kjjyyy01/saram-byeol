02ac76d8824a18b204fa0a898bc9be75173f6eb0
Jongyeon Kim <tyjysong0712@gmail.com>
2025-04-21 21:07:37 +0900

    style:#119 로그인 페이지의 여백 및 텍스트 스타일 수정

--- src/app/(pages)/signIn/page.tsx
+++ src/app/(pages)/signIn/page.tsx
@@ -11,7 +11,7 @@ const SignIn = () => {
     <div className='flex h-[100vh] w-[100vw] items-center justify-center overflow-hidden overflow-y-auto'>
       <section className='flex w-full flex-col items-center justify-center'>
         <h1 className='mb-10 text-center text-[28px] font-bold text-primary-500'>사람, 별 로그인</h1>
-        <section className='mb-8'>
+        <section className='mb-[22px]'>
           <SigninForm />
         </section>
 
@@ -24,7 +24,7 @@ const SignIn = () => {
           </div>
           <div className='mb-[42px] flex items-center gap-1'>
             <div className='w-[150px] outline outline-1 outline-stone-300' />
-            <p className='text-nowrap text-base font-bold leading-normal text-zinc-800'>SNS계정으로 간편로그인</p>
+            <p className='text-nowrap font-bold leading-normal text-zinc-800'>SNS계정으로 간편로그인</p>
             <div className='h-0 w-[150px] outline outline-1 outline-stone-300' />
           </div>
           <SigninSocial />
