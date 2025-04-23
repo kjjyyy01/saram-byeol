30cf4b1a0220ab1b4cb3d2c1380e025ad9151484
DnJ0408 <oyj921205@gmail.com>
2025-04-23 16:04:24 +0900

    style:#134 폼 제출 버튼 css 변경

--- src/components/contacts/addContactForm/ContactFormSubmitButton.tsx
+++ src/components/contacts/addContactForm/ContactFormSubmitButton.tsx
@@ -7,11 +7,14 @@ interface ContactFormSubmitButtonProps {
 
 const ContactFormSubmitButton = ({ isSubmitting }: ContactFormSubmitButtonProps) => {
   return (
-    <div className='flex justify-end pt-10'>
-      <Button type='submit' disabled={isSubmitting}>
-        {isSubmitting ? '추가 중...' : '추가'}
-      </Button>
-    </div>
+    <Button
+      type='submit'
+      disabled={isSubmitting}
+      className='min-h-12 flex-1 bg-primary-500 px-10 py-4 text-center font-bold text-grey-0 transition hover:bg-primary-600 active:bg-primary-700'
+      variant='default'
+    >
+      {isSubmitting ? '저장 중...' : '저장'}
+    </Button>
   );
 };
 
