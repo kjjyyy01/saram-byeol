cc0bacaa3e423c60c1ffaa277164b20ade9cdf20
lee kiri <mango.man.e.ya@gmail.com>
2025-04-23 14:26:12 +0900

    refactor#117 LeftNavBar 컴포넌트에서 경로 상수 수정 및 코드 정리

--- src/components/nav/LeftNavBar.tsx
+++ src/components/nav/LeftNavBar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { CALENDER, PEOPLE, SIGNIN } from '@/constants/paths';
+import { CALENDER, HOME, PEOPLE, SIGNIN } from '@/constants/paths';
 import { Users, CalendarBlank, SignOut, Bell } from '@phosphor-icons/react';
 import { useAuthStore } from '@/store/zustand/store';
 import Image from 'next/image';
@@ -16,9 +16,9 @@ const LeftNavBar = () => {
   const accessUser = isDemoUser ? demoUser : user;
   const provider = accessUser?.app_metadata?.provider;
   const page = usePathname().slice(1);
-  const profile = accessUser?.user_metadata?.profile_img || PLACEHOLDER_PROFILE_IMG; 
+  const profile = accessUser?.user_metadata?.profile_img || PLACEHOLDER_PROFILE_IMG;
   const isAccessGranted = isSignIn || isDemoUser;
-  
+
   const logoutHandler = async () => {
     if (isDemoUser) {
       toast.info('데모 체험을 종료합니다');
@@ -36,55 +36,64 @@ const LeftNavBar = () => {
   };
 
   return (
-    <nav className='flex flex-col'>
-      <Link href={PEOPLE}>
-        <div
-          className={`flex h-[150px] w-40 flex-col items-center justify-center gap-2 bg-grey-0 px-5 ${page === 'people' ? 'pointer-events-none cursor-default bg-primary-500 text-grey-0' : 'hover: cursor-pointer hover:bg-grey-50'}`}
-        >
-          <Users size={24} />
-          내사람
+    <nav className='flex h-screen'>
+      <div className='flex flex-col items-center justify-between py-16'>
+        <div>
+          <Link href={HOME}>
+            <Image src={'/app-logo.png'} width={80} height={80} alt='app logo' />
+          </Link>
         </div>
-      </Link>
-      <Link href={CALENDER}>
-        <div
-          className={`flex h-[150px] w-40 flex-col items-center justify-center gap-2 bg-grey-0 px-5 ${page === 'calendar' ? 'pointer-events-none cursor-default bg-primary-500 text-grey-0' : 'hover: cursor-pointer hover:bg-grey-50'}`}
-        >
-          <CalendarBlank size={24} />
-          캘린더
-        </div>
-      </Link>
-      <div
-        className={`nav-cell hover: flex h-[150px] w-40 cursor-pointer flex-col items-center justify-center gap-2 px-5 hover:bg-grey-50`}
-        onClick={logoutHandler}
-      >
-        <SignOut size={24} />
-        {isDemoUser ? '데모 종료' : '로그아웃'}
-      </div>
-
-      {/* 로그인된 상태일 때만 보여줌 */}
-      {isAccessGranted ? (
-        <div className='flex h-[150px] w-40 flex-col items-center justify-center gap-6 px-5 text-grey-1000'>
-          <Bell size={24} />
-          <div className='flex flex-row items-center justify-center gap-2'>
-            <Image
-              src={profile}
-              width={48}
-              height={48}
-              alt='유저 프로필 이미지'
-              className='h-12 w-12 rounded-full object-cover'
-            />
-            {provider !== 'email' ? (
-              <div>{accessUser?.user_metadata?.name}</div>
-            ) : (
-              <div>{accessUser?.user_metadata?.nickname}</div>
-            )}
+        <div>
+          <Link href={PEOPLE}>
+            <div
+              className={`flex h-[150px] w-40 flex-col items-center justify-center gap-2 bg-grey-0 px-5 ${page === 'people' ? 'pointer-events-none cursor-default bg-primary-500 text-grey-0' : 'hover: cursor-pointer hover:bg-grey-50'}`}
+            >
+              <Users size={24} />
+              내사람
+            </div>
+          </Link>
+          <Link href={CALENDER}>
+            <div
+              className={`flex h-[150px] w-40 flex-col items-center justify-center gap-2 bg-grey-0 px-5 ${page === 'calendar' ? 'pointer-events-none cursor-default bg-primary-500 text-grey-0' : 'hover: cursor-pointer hover:bg-grey-50'}`}
+            >
+              <CalendarBlank size={24} />
+              캘린더
+            </div>
+          </Link>
+          <div
+            className={`nav-cell hover: flex h-[150px] w-40 cursor-pointer flex-col items-center justify-center gap-2 px-5 hover:bg-grey-50`}
+            onClick={logoutHandler}
+          >
+            <SignOut size={24} />
+            {isDemoUser ? '데모 종료' : '로그아웃'}
           </div>
         </div>
-      ) : (
-        <Link href={SIGNIN}>
-          <div className='flex h-[150px] w-40 items-center justify-center hover:bg-grey-50'>로그인하러가기</div>
-        </Link>
-      )}
+        <div>
+          {isAccessGranted ? (
+            <div className='flex h-[150px] w-40 flex-col items-center justify-center gap-6 px-5 text-grey-1000'>
+              <Bell size={24} />
+              <div className='flex flex-row items-center justify-center gap-2'>
+                <Image
+                  src={profile}
+                  width={40}
+                  height={40}
+                  alt='유저 프로필 이미지'
+                  className='h-10 w-10 rounded-full object-cover'
+                />
+                {provider !== 'email' ? (
+                  <div>{accessUser?.user_metadata?.name}</div>
+                ) : (
+                  <div>{accessUser?.user_metadata?.nickname}</div>
+                )}
+              </div>
+            </div>
+          ) : (
+            <Link href={SIGNIN}>
+              <div className='flex h-[150px] w-40 items-center justify-center hover:bg-grey-50'>로그인하러가기</div>
+            </Link>
+          )}
+        </div>
+      </div>
     </nav>
   );
 };
