449cb46bd1f93a875522915bfdd971d53cbdf0da
lee kiri <mango.man.e.ya@gmail.com>
2025-04-24 01:43:13 +0900

    feat#143 MainDoor 컴포넌트 주석 처리 및 LandingPage로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import MainDoor from '@/components/home/MainDoor';
+import LandingPage from '@/components/home/LandingPage';
+// import MainDoor from '@/components/home/MainDoor';
 import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <MainDoor />
+    <div className={`overflow-hidden [&::-webkit-scrollbar]:hidden`}>
+      {/* <MainDoor /> */}
+      <LandingPage />
     </div>
   );
 };
