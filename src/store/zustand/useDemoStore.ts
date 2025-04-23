e2490beea729f067589923bb05a9f25f5236e47c
lee kiri <mango.man.e.ya@gmail.com>
2025-04-23 12:32:27 +0900

    refactor#117 데모 사용자 관련 불필요한 코드 제거

--- src/store/zustand/useDemoStore.ts
+++ src/store/zustand/useDemoStore.ts
@@ -2,7 +2,6 @@ import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 import type { ContactType } from '@/types/contacts';
 import type { PlansType, SelectPlanType } from '@/types/plans';
-import { toast } from 'react-toastify';
 import { demoContacts, demoPlans, demoUser } from '@/constants/demoData';
 import { User } from '@supabase/supabase-js';
 
@@ -13,19 +12,12 @@ interface DemoState {
   demoUser: User;
 
   setDemoUser: () => void;
-  addContact: (person: ContactType) => void;
-  updateContact: (person: ContactType) => void;
-  togglePinContact: (id: string, isPinned: boolean) => void;
-  deleteContact: (id: string) => void;
+
   getContactsWithPlans: (id: string) => {
     contact: ContactType;
     plans: PlansType[];
   };
   getPlan: (id: string) => { data: SelectPlanType[]; error: null };
-  addPlan: (plan: PlansType) => void;
-  updatePlan: (plan: PlansType) => void;
-  deletePlan: (id: string) => void;
-
   clearAll: () => void;
 }
 
@@ -47,44 +39,12 @@ export const useDemoStore = create<DemoState>()(
           localStorage.setItem('user-storage', JSON.stringify(demoUser));
           return { isDemoUser: true };
         }),
-      addContact: (person) =>
-        set((state) => {
-          if (state.demoContacts.length >= 3) {
-            toast.warning('데모모드에서는 최대 3명까지 등록할 수 있습니다.');
-            return state;
-          }
-          return { demoContacts: [...state.demoContacts, person] };
-        }),
       getContactsWithPlans: (id) => {
         const contact = get().demoContacts.find((c) => c.contacts_id === id)!;
         const plans = get().demoPlans.filter((p) => p.contacts_id === id);
 
         return { contact, plans };
       },
-
-      updateContact: (person) =>
-        set((state) => ({
-          demoContacts: state.demoContacts.map((p) => (p.id === person.id ? person : p)),
-        })),
-      togglePinContact: (id, isPinned) => {
-        let updatedContacts: ContactType[] = [];
-
-        set((state) => {
-          updatedContacts = state.demoContacts.map((p) => (p.id === id ? { ...p, is_pinned: isPinned } : p));
-
-          return {
-            demoContacts: updatedContacts,
-          };
-        });
-
-        return { updatedContacts };
-      },
-      deleteContact: (id) =>
-        set((state) => ({
-          demoContacts: state.demoContacts.filter((p) => p.id !== id),
-          demoPlans: state.demoPlans.filter((a) => a.plan_id !== id),
-        })),
-
       getPlan: (id) => {
         const plans = get().demoPlans.filter((p) => p.plan_id === id);
 
@@ -103,23 +63,6 @@ export const useDemoStore = create<DemoState>()(
           error: null,
         };
       },
-      addPlan: (plan) =>
-        set((state) => {
-          if (state.demoPlans.length >= 5) {
-            toast.warning('데모 모드에서는 최대 5개의 약속만 등록할 수 있습니다.');
-            return state;
-          }
-          return { demoPlans: [...state.demoPlans, plan] };
-        }),
-      updatePlan: (plan) =>
-        set((state) => ({
-          demoPlans: state.demoPlans.map((p) => (p.plan_id === plan.plan_id ? { ...p, plan } : p)),
-        })),
-      deletePlan: (id) =>
-        set((state) => ({
-          demoPlans: state.demoPlans.filter((a) => a.plan_id !== id),
-        })),
-
       clearAll: () =>
         set(() => {
           localStorage.removeItem('user-storage');
