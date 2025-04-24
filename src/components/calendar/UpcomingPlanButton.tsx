626233faa935c41719a4acc41f8cfd3cc4a1ea21
JinChaeKim <rlawlsco88@gmail.com>
2025-04-24 20:43:54 +0900

    style:#156 색상 primary로 변경

--- src/components/calendar/UpcomingPlanButton.tsx
+++ src/components/calendar/UpcomingPlanButton.tsx
@@ -28,7 +28,9 @@ const UpcomingPlanButton = ({ onClick, activeTab }: Props) => {
         className={`mr-6 rounded-md border-[1px] px-5 py-3 text-[14px] font-bold ${activeTab === 'upcoming' ? 'border-primary-500 text-primary-500' : 'border-grey-300 text-grey-700'}`}
       >
         다가오는 약속
-        <span className={`m-1 rounded-full px-1 text-white ${activeTab === 'upcoming' ? 'bg-[#0066FF]' : 'bg-black'}`}>
+        <span
+          className={`m-1 rounded-full px-1 text-white ${activeTab === 'upcoming' ? 'bg-primary-500' : 'bg-black'}`}
+        >
           {upcomingCount !== null ? upcomingCount : ''}
         </span>
       </button>
