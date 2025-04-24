2cf3a71f1e542d994fc6fbe95b799a9e6b9786d7
lee kiri <mango.man.e.ya@gmail.com>
2025-04-25 00:13:26 +0900

    feat#143 MainHeader 컴포넌트에서 버튼을 링크로 변경하고 데모 체험 기능 추가

--- src/components/home/MainHeader.tsx
+++ src/components/home/MainHeader.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image';
 import React from 'react';
-import { Button } from '../ui/button';
 import Link from 'next/link';
-import { SIGNIN } from '@/constants/paths';
+import { PEOPLE, SIGNIN } from '@/constants/paths';
+import { useDemoStore } from '@/store/zustand/useDemoStore';
 
 interface Props {
   onNavClick: (key: 'about' | 'function' | 'subscription') => void;
 }
+
 const MainHeader = ({ onNavClick }: Props) => {
+  const setDemoUser = useDemoStore((state) => state.setDemoUser);
   return (
     <div className='fixed box-border flex h-24 w-full items-center justify-center'>
       <div className='container flex flex-row items-center justify-between px-6'>
@@ -16,7 +18,7 @@ const MainHeader = ({ onNavClick }: Props) => {
           alt='app-logo-icon'
           width={88}
           height={85}
-          className='shadow-app-logo rounded-[20px]'
+          className='rounded-[20px] shadow-app-logo'
         />
         <div className='flex flex-row items-center justify-center gap-14 text-2xl font-bold'>
           <div onClick={() => onNavClick('about')} className='cursor-pointer'>
@@ -31,9 +33,13 @@ const MainHeader = ({ onNavClick }: Props) => {
         </div>
         <div className='flex flex-row items-center justify-center gap-6'>
           <Link href={SIGNIN}>로그인</Link>
-          <Button className='box-border h-14 w-[180px] rounded-lg bg-primary-500 px-6 py-4 font-bold hover:bg-primary-600 active:bg-primary-700'>
-            지금 무료로 시작하기
-          </Button>
+          <Link
+            href={PEOPLE}
+            className='box-border h-14 w-[180px] rounded-lg bg-primary-500 px-6 py-4 text-center font-bold text-grey-0 hover:bg-primary-600 active:bg-primary-700'
+            onClick={setDemoUser}
+          >
+            지금 데모 체험하기
+          </Link>
         </div>
       </div>
     </div>
