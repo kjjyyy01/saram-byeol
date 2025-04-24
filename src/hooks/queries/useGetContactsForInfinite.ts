75b1836b38ca76d76b5230f0e9a5bead4aee565c
DnJ0408 <oyj921205@gmail.com>
2025-04-24 14:18:24 +0900

    docs:#87 as const 삭제

--- src/hooks/queries/useGetContactsForInfinite.ts
+++ src/hooks/queries/useGetContactsForInfinite.ts
@@ -1,19 +1,18 @@
-import { fetchPinnedContacts } from '@/app/api/supabase/service';
+import { fetchPinnedContacts, fetchRegularContactsInfinite } from '@/app/api/supabase/service';
 import { QUERY_KEY } from '@/constants/queryKey';
-import { fetchRegularContactsInfinite } from '@/lib/services/contactsService';
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 
 // 고정된 연락처 한 번만 조회
 export const usePinnedContacts = (userId: string) =>
   useQuery({
-    queryKey: [QUERY_KEY.CONTACTS, 'pinned', userId] as const,
+    queryKey: [QUERY_KEY.CONTACTS, 'pinned', userId],
     queryFn: () => fetchPinnedContacts(userId),
   })
 
 // 일반 연락처 무한 스크롤
 export const useRegularContactsInfinite = (userId: string) =>
   useInfiniteQuery({
-    queryKey: [QUERY_KEY.CONTACTS_INFINITE, 'regular', userId] as const,
+    queryKey: [QUERY_KEY.CONTACTS_INFINITE, 'regular', userId],
     queryFn: ({ pageParam = 0 }) =>
       fetchRegularContactsInfinite(userId, pageParam),
     getNextPageParam: lastPage => lastPage.nextPage,
