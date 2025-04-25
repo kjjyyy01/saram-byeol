733e91fa4ca049f8990af8c9c5e55e0705d5ee5b
Jongyeon Kim <tyjysong0712@gmail.com>
2025-04-25 15:06:31 +0900

    feat:#160 비밀번호 확인 필드 일치 여부 검증 로직 추가

--- src/lib/schemas/changePasswordSchema.ts
+++ src/lib/schemas/changePasswordSchema.ts
@@ -1,13 +1,18 @@
 import { z } from 'zod';
 
-export const changePasswordSchema = z.object({
-  password: z
-    .string()
-    .min(1, { message: '비밀번호를 입력해주세요.' })
-    .regex(/[!@#$%^&*]/, {
-      message: '하나 이상의 특수문자가 포함되어야 합니다.',
-    })
-    .min(8, { message: '8자 이상으로 입력해주세요.' }),
+export const changePasswordSchema = z
+  .object({
+    password: z
+      .string()
+      .min(1, { message: '비밀번호를 입력해주세요.' })
+      .regex(/[!@#$%^&*]/, {
+        message: '하나 이상의 특수문자가 포함되어야 합니다.',
+      })
+      .min(8, { message: '8자 이상으로 입력해주세요.' }),
 
-  passwordCheck: z.string().min(1, { message: '비밀번호를 다시 입력해주세요.' }),
-});
+    passwordCheck: z.string().min(1, { message: '비밀번호를 다시 입력해주세요.' }),
+  })
+  .refine((data) => data.password === data.passwordCheck, {
+    path: ['passwordCheck'],
+    message: '비밀번호가 일치하지 않습니다.',
+  });
