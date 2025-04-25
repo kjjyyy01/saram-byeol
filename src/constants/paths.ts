cbcfd42f3c9c4ac193c2d3a65721a4e3f70e8ea8
Jongyeon Kim <tyjysong0712@gmail.com>
2025-04-25 12:41:00 +0900

    feat:#160 forgot-password 및 change-password 페이지 경로 상수 추가

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -3,3 +3,5 @@ export const SIGNIN = '/signIn';
 export const SIGNUP = '/signUp';
 export const PEOPLE = '/people';
 export const CALENDER = '/calendar';
+export const FORGOT_PASSWORD = '/forgot-password';
+export const CHANGE_PASSWORD = '/change-password';
