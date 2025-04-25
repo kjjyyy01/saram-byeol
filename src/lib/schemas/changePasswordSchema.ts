7250f801a6e3e54097ec6b42348c0f2b98ac10b8
Jongyeon Kim <tyjysong0712@gmail.com>
2025-04-25 15:01:13 +0900

    feat:#160 비밀번호 변경을 위한 유효성 검사 스키마 추가

---
+++ src/lib/schemas/changePasswordSchema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const changePasswordSchema = z.object({
+  password: z
+    .string()
+    .min(1, { message: '비밀번호를 입력해주세요.' })
+    .regex(/[!@#$%^&*]/, {
+      message: '하나 이상의 특수문자가 포함되어야 합니다.',
+    })
+    .min(8, { message: '8자 이상으로 입력해주세요.' }),
+
+  passwordCheck: z.string().min(1, { message: '비밀번호를 다시 입력해주세요.' }),
+});
