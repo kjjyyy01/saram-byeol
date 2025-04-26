a82dd64527fe6d5ea3da4300cb760c5e1a73e7db
JinChaeKim <rlawlsco88@gmail.com>
2025-04-26 21:04:36 +0900

    refactor:#156 에러 컴포넌트 수정 - supabase 통신 로직에 적은 디테일 에러 메세지 포함

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,19 +1,22 @@
 import { Warning } from '@phosphor-icons/react';
 
 type GenericErrorProps = {
-  message?: string;
   error?: Error | null;
+  originalError: Error | null;
   refetch?: () => void;
 };
 
-export default function GenericError({ message, error, refetch }: GenericErrorProps) {
+export default function GenericError({ error, refetch, originalError }: GenericErrorProps) {
   return (
     <div className='flex min-h-screen flex-col items-center justify-center gap-8'>
       <Warning size={60} className='text-secondary-600' />
-      <h2 className='title text-[45px] font-bold text-primary-700'>에러 발생!</h2>
-      <p className='text-[23px] text-primary-500'>
-        {message} {error?.message}
-      </p>
+      <h2 className='title text-[45px] font-bold text-primary-700'>Error!</h2>
+      {error?.message && originalError?.message && (
+        <section className='flex flex-col items-center text-[23px] text-primary-500'>
+          <p>{error?.message}</p>
+          <p>Detail: {originalError?.message}</p>
+        </section>
+      )}
       {refetch && (
         <button
           onClick={refetch}
