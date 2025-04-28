2fbb31789ba77c553cad53e9ee482c2039c4ae51
Jongyeon Kim <tyjysong0712@gmail.com>
2025-04-28 20:36:20 +0900

    feat:#160 이메일 인증 및 회원가입 완료, 비밀번호 재설정 대기 경로 상수 추가

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -5,3 +5,7 @@ export const PEOPLE = '/people';
 export const CALENDER = '/calendar';
 export const FORGOT_PASSWORD = '/forgot-password';
 export const CHANGE_PASSWORD = '/change-password';
+export const CONFIRM_EMAIL = '/confirm-email';
+export const FINISH_SIGNUP = '/finish-signup';
+export const AUTH_CALLBACK = '/auth/callback';
+export const WAIT_CHANGE_PASSWORD = '/wait-change-password';
