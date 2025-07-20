480fecf7d258576c73de9548d976208cfe6f4cef
Jongyeon <tyjysong0712@gmail.com>
2025-07-20 14:29:49 +0900

    feat:#13 이미지 태그에 fetchPriority 속성 추가

--- src/app/(pages)/forgot-password/page.tsx
+++ src/app/(pages)/forgot-password/page.tsx
@@ -80,6 +80,8 @@ const ForgotPassword = () => {
           width={950}
           height={1080}
           className='mx-auto object-contain'
+          priority
+          fetchPriority='high'
         />
       </section>
     </div>
